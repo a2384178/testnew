7225cd4ad38a4dcb1cc1b0d2e9d8ce74f955fcfe
a2384178 <a2384178@163.com>
2017-04-11 23:59:46 +0800

    增加进度条

---
+++ a.js
@@ -0,0 +1,22 @@
+function domore(obj,attr,dir,target,endFu){
+    dir=parseInt(getstyle(obj,attr))<target?dir:-dir;
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function () {
+        var speed=parseInt(getstyle(obj,attr))+dir;
+        if(speed>target&&dir>0||speed<target&&dir<0){
+            speed=target;
+        }
+
+        obj.style[attr]=speed+'px';
+        if(speed==target){
+            clearInterval(obj.timer);
+            /*if(endFu){
+             endFu()
+             }*/
+            endFu&&endFu();//与上面的方法等价
+        }
+    },30);
+}
+function getstyle(obj,attr) {return obj.currentStyle?obj.currentStyle[attr]:getComputedStyle(obj)[attr];
+
+}
